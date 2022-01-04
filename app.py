eefe8d2f8c0ac6d6c18783cff8130fec98f08c76
mo-root <59801139+mo-root@users.noreply.github.com>
2022-01-04 11:27:54 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, redirect
+import pyrebase
+
+config = {
+  "apiKey": "AIzaSyC6_SRpLMbZtV7I5ZT32w8vjDwXO-6wqs4",
+  "authDomain": "aster4u-a47b9.firebaseapp.com",
+  "databaseURL": "https://aster4u-a47b9-default-rtdb.europe-west1.firebasedatabase.app",
+  "projectId": "aster4u-a47b9",
+  "storageBucket": "aster4u-a47b9.appspot.com",
+  "messagingSenderId": "934119027505",
+  "appId": "1:934119027505:web:e6376319c0a29a5fa0b610",
+  "measurementId": "G-NTYT2E23LX"
+}
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+db = firebase.database()
+
+
+
+app = Flask(__name__)
+#Bootstrap(app)
+
+
+app.config['SECRET_KEY'] = 'you-will-never-guess'
+
+
+
+
+@app.route('/')
+def fullList1():
+    return 'welcome'
+
+
+@app.route('/<string:topic>')
+def getS(topic):
+    topic = db.child("Links").child(topic).get().val()
+    return topic
+
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
